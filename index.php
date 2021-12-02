be47a302de6e537703b3b62dcf7f0c93ad5be352
Noah <noah.heidrich@gmail.com>
2021-12-02 16:41:09 +0100

    Anmelde und Registrationsbutton nach oben

--- index.php
+++ index.php
@@ -52,6 +52,24 @@
             <li class="nav-item">
               <a href="#kontakt" class="nav-link text-warning">Kontakt</a>
             </li>
+            <li class="nav-item">
+              <button
+                class="btn btn-warning btn-sm mx-4"
+                data-bs-toggle="modal"
+                data-bs-target="#anmelden"
+              >
+                Hier anmelden
+              </button>
+            </li>
+            <li class="nav-item">
+              <button
+                class="btn btn-warning btn-sm"
+                data-bs-toggle="modal"
+                data-bs-target="#registrieren"
+              >
+                Hier registrieren
+              </button>
+            </li>
           </ul>
         </div>
       </div>
@@ -70,13 +88,7 @@
               Entdecke mit uns ungeahnte Talente und entfalte dein Potential!
 
             </p>
-            <button
-              class="btn btn-warning btn-lg"
-              data-bs-toggle="modal"
-              data-bs-target="#enroll"
-            >
-              Hier anmelden
-            </button>
+
           </div>
           <img
             class="img-fluid w-25 d-none d-sm-block"
@@ -487,7 +499,7 @@
     <!-- Modal Hier Anmelden -->
     <div
       class="modal fade"
-      id="enroll"
+      id="anmelden"
       tabindex="-1"
       aria-labelledby="enrollLabel"
       aria-hidden="true"
@@ -495,7 +507,7 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="enrollLabel">Enrollment</h5>
+            <h5 class="modal-title" id="enrollLabel">Anmelden</h5>
             <button
               type="button"
               class="btn-close"
@@ -504,25 +516,71 @@
             ></button>
           </div>
           <div class="modal-body">
-            <p class="lead">Fill out this form and we will get back to you</p>
             <form>
               <div class="mb-3">
-                <label for="first-name" class="col-form-label">
-                  First Name:
+                <label for="email" class="col-form-label">
+                  Email
                 </label>
-                <input type="text" class="form-control" id="first-name" />
+                <input type="text" class="form-control" id="email" />
               </div>
               <div class="mb-3">
-                <label for="last-name" class="col-form-label">Last Name:</label>
-                <input type="text" class="form-control" id="last-name" />
+                <label for="password" class="col-form-label">Passwort:</label>
+                <input type="text" class="form-control" id="password" />
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal Hier Registrieren -->
+    <div
+      class="modal fade"
+      id="registrieren"
+      tabindex="-1"
+      aria-labelledby="enrollLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="enrollLabel">Registrieren</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="mb-3">
+                <label for="email" class="col-form-label">
+                  Email
+                </label>
+                <input type="text" class="form-control" id="email" />
               </div>
               <div class="mb-3">
-                <label for="email" class="col-form-label">Email:</label>
-                <input type="email" class="form-control" id="email" />
+                <label for="password" class="col-form-label">Passwort:</label>
+                <input type="text" class="form-control" id="password" />
               </div>
               <div class="mb-3">
-                <label for="phone" class="col-form-label">Phone:</label>
-                <input type="tel" class="form-control" id="phone" />
+                <label for="vorname" class="col-form-label">Vorname:</label>
+                <input type="text" class="form-control" id="vorname" />
+              </div>
+              <div class="mb-3">
+                <label for="telefonnummer" class="col-form-label">Telefonnummer:</label>
+                <input type="text" class="form-control" id="telefonnummer" />
               </div>
             </form>
           </div>
