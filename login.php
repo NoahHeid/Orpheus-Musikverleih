c91aeb4d752a6f06b18b7c2cdd44452a672f5fa2
Noah <noah.heidrich@gmail.com>
2021-12-19 22:59:20 +0100

    Login nun sauberer

--- login.php
+++ login.php
@@ -22,20 +22,17 @@ if($connection->connect_error){
 $sql = "SELECT * FROM kunden WHERE kd_email = '$email' AND kd_kennwort = '$pass'";
 $ergebnis = $connection ->query($sql);
 
-if($ergebnis->num_rows > 0){
-    while($i = $ergebnis->fetch_assoc()){
-
-        echo "Hallo: ID:".$i["kd_id"]." Name: ".$i["kd_vorname"]." ".$i["kd_nachname"];
-        $_SESSION['loggedin'] = true;
-        $_SESSION['id'] = $i["kd_id"];
-        $_SESSION['vorname'] = $i["kd_vorname"];
-        $_SESSION['nachname'] = $i["kd_nachname"];
-        $_SESSION['email'] = $i["kd_email"];
-        $_SESSION['handy'] = $i["kd_handy"];
-    }
+if($ergebnis->num_rows == 1){
+    $i = $ergebnis->fetch_assoc();
+    echo "Hallo: ID:".$i["kd_id"]." Name: ".$i["kd_vorname"]." ".$i["kd_nachname"];
+    $_SESSION['loggedin'] = true;
+    $_SESSION['id'] = $i["kd_id"];
+    $_SESSION['vorname'] = $i["kd_vorname"];
+    $_SESSION['nachname'] = $i["kd_nachname"];
+    $_SESSION['email'] = $i["kd_email"];
+    $_SESSION['handy'] = $i["kd_handy"];
     //Schliesse Datenbank!
     $connection->close();
-
     header('Location: '."index.php");
     die();
 }
