a65ff01a9adc05963d2d5358c6c3393d6277d437
Noah <noah.heidrich@gmail.com>
2022-01-08 18:35:23 +0100

    Ausleihe

--- ausleihe.php
+++ ausleihe.php
@@ -49,39 +49,35 @@
       <button type="button" data-bs-target="#carouselInstruments" data-bs-slide-to="3" aria-label="Slide 4"></button>
     </div>
 
-    <div class="carousel-item active" data-bs-interval="4000"> 
-        <div class="col-6">
-          <img src="img/instrumente/höfner_student_violine.png" class="img-fluid" alt="...">
-        </div>
-        <div class="col-6">
-          Test
-        </div>
-      
-    </div>
-
-    <div class="carousel-item " data-bs-interval="2500">
+    <div class="carousel-item active" data-bs-interval="4000">
       <div class="carousel-caption d-none d-md-block">
-        <img src="img/geige1.png" class="d-block w-100" alt="...">
-        <h5>Scala Vilagio F.H. Maggini</h5>
-            <p>Eine wunderschön gearbeitete Violine italienischer Machart mit Griffbrett aus Ebenholz und geflammtem Boden.
-
-            </p>
+        <img src="img/instrumente/höfner_student_violine.png" class="d-block w-100" alt="...">
+        <h5>HÖFNER Student Violine</h5>
+        <p>Ausgezeichnete Schülergeige aus dem Hause der deutschen Geigenbauwerkstatt Karl Höfner</p>
       </div>
     </div>
 
     <div class="carousel-item" data-bs-interval="2500">
       <div class="carousel-caption d-none d-md-block">
-        <img src="img/geige2.png" class="d-block w-100" alt="...">
-        <h5>Gewa Pure Violaset HW 40,8cm</h5>
-            <p>Eine gut gearbeitete Viola mit Griffbrett aus Hartholz. Das Gewa Pure Violaset enthält einen Bogen und glänzt durch einen warmen und vollen Klang.</p>
+        <img src="img/instrumente/pacato_evioline.png" class="d-block w-100" alt="...">
+        <h5>PACATO E-Violine</h5>
+        <p>Elektrische Violine mit schwarzem Teilkorpus und Stahlseiten</p>
       </div>
     </div>
 
     <div class="carousel-item" data-bs-interval="2500">
       <div class="carousel-caption d-none d-md-block">
-        <img src="img/geige3.png" class="d-block w-100" alt="...">
-        <h5>Conrad Götz Heritage Contempor. 115 Violin</h5>
-            <p>Violine deutscher Machart von sehr hoher Qualität. Conrad Götz Heritage hat ein Griffbrett aus Ebenholz und überzeugt mit feinen und eleganten Klängen</p>
+        <img src="img/instrumente/paganini_allegro.png" class="d-block w-100" alt="...">
+        <h5>PAGANINO Allegro Violine</h5>
+        <p>Schöne Schülergeige von überzeugender Qualität mit Garnitur und Kinnhalter aus Ebenholz</p>
+      </div>
+    </div>    
+    
+    <div class="carousel-item" data-bs-interval="2500">
+      <div class="carousel-caption d-none d-md-block">
+        <img src="img/instrumente/paganino_classic.png" class="d-block w-100" alt="...">
+        <h5>PAGANINO Classic Viola</h5>
+        <p>Sehr gute Schülerbratsche von überzeugender Qualität aus handausgearbeitem Massivholz</p>
       </div>
     </div>
 
@@ -102,80 +98,39 @@
 
 <!-- Geigenverleih -->
 <section class="text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start bgmaincolor5 border border-dark">
-  <div class="container">
-    <div class="row d-sm-flex align-items-around">
-      <!-- Geigenbild -->
-      <div class="col-5">
-        <img src="img/geigenAusleihe.png" class="img-fluid" alt="Responsive image">
-      </div>
-      <div class="col-2"></div>
-      <!-- Geigentabelle -->
-      <div class="col-5">
-        <table class="table text-dark mr-5">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
-          <!-- Tabellenhead -->
-          <thead>
-            <tr>
-              <th scope="col">Geige</th>
-              <th scope="col">Ausleihen</th>
-            </tr>
-          </thead>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="la.jpg" alt="Los Angeles">
+    </div>
 
-          <!-- Tabellenbody -->
-          <tbody>
-            <?php
-              $geigenSQL = "SELECT * FROM geigen";
-              $geigenDaten = SQL($geigenSQL);
-              foreach ($geigenDaten as $geige) 
-              {
-            ?>
-              <tr>
-                <td>
-                    <?php echo $geige->gg_name; ?>
-                </td>  
-                <td>
-                  <?php 
-                    //Falls die Geige auf Lager ist (Also das Ausleihdatum NULL ist), mache es möglich Auszuleihen!
-                    if($geige->gg_ausleihdatum == NULL){
-                      echo '<form method="post" action="server/geigen.php">
-                      <div class="input-group mb-3">
-                        <input type="text" hidden name="ggID" value='.$geige->gg_id.'>
-                        <input type="text" hidden name="kdID" value='.$_SESSION['id'].'>
-                        <input type="submit" class="btn-sm bg-transparent btn-outline-primary"  value="Ausleihen" name="ausleihen" id="ausleihen">
-                      </div>
-                    </form>';
-                    }else{
-                      //Prüfe ob das Element vom Nutzer ausgeliehen ist, wenn ja, mach es möglich, das Element zurückzugeben!
-                      if($geige->kd_id == $_SESSION['id']){
-                        echo '<form method="post" action="server/geigen.php">
-                        <div class="input-group mb-3">
-                          <input type="text" hidden name="ggID" value='.$geige->gg_id.'>';
-                          $date = new DateTime();
-                          if($date->getTimestamp() - strtotime($geige->gg_ausleihdatum)>604800){
-                            echo '<input type="submit" class="btn-sm bg-transparent btn-outline-danger"  value="Zurückgeben (Überfällig!)" name="zurückgeben" id="zurückgeben">';
-                          }
-                          else{
-                            echo '<input type="submit" class="btn-sm bg-transparent btn-outline-primary"  value="Zurückgeben" name="zurückgeben" id="zurückgeben">';
-                          }
-                          echo '
-                        </div>
-                      </form>';
-                      }
-                      else{
-                        echo 'Bereits seit '.date("d.m.y", strtotime($geige->gg_ausleihdatum)).' ausgeliehen!';
-                      }
-                    }
-                  ?>
-                </td>
-              </tr>
-            <?php
-            }
-            ?>
-          </tbody>
-        </table>
-      </div>
+    <div class="item">
+      <img src="chicago.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="ny.jpg" alt="New York">
     </div>
   </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </section>
 
 <!-- Harfenverleih -->
