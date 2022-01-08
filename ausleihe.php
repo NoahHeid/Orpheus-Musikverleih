5102cb38553365186a630dcdf97348ba8942af98
moritzhussing <94633277+moritzhussing@users.noreply.github.com>
2022-01-08 13:56:05 +0100

    nowGendered

--- ausleihe.php
+++ ausleihe.php
@@ -23,7 +23,7 @@
   <div class="container">
     <div class="d-sm-flex align-items-center justify-content-between">
       <div>
-        <h1> <?php echo "Lieber ".$_SESSION['vorname']?>, viel Spaß beim Erkunden unseres <span class="text-warning"> Instrumentenparadises!</span></h1>
+        <h1> <?php echo "Liebe*r ".$_SESSION['vorname']?>, viel Spaß beim Erkunden unseres <span class="text-warning"> Instrumentenparadises!</span></h1>
         <p class="lead my-4">
             Bitte denk daran, dass die Instrumente nach einer Woche zurückgegeben werden müssen!
         </p>
@@ -180,49 +180,6 @@
     </div>
   </div>
 </section>
-
-<section class="awSlider">
-  <div  class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target=".carousel" data-slide-to="0" class="active"></li>
-      <li data-target=".carousel" data-slide-to="1"></li>
-      <li data-target=".carousel" data-slide-to="2"></li>
-      <li data-target=".carousel" data-slide-to="3"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-941.jpg">
-        <div class="carousel-caption">Harfe #1</div>
-      </div>
-      <div class="item">
-        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-101.jpg">
-        <div class="carousel-caption">Geige #2</div>
-      </div>
-      <div class="item">
-        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-1051.jpg">
-        <div class="carousel-caption">Harfe #3</div>
-      </div>
-      <div class="item">
-        <img src="http://www.adobewordpress.com/wp-content/uploads/2013/07/wallpaper-thumb-74.jpg">
-        <div class="carousel-caption">Geige #4</div>
-      </div>
-    </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href=".carousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Geri</span>
-    </a>
-    <a class="right carousel-control" href=".carousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">İleri</span>
-    </a>
-  </div>
-</section>
-
 <?php
   include 'statisch/footer.php';
 ?>
